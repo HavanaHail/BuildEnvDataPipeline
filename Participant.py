5af4a007134a3faea09972667b4fee301956ac5b
nwhalen <nwhalen@wpi.edu>
2023-02-14 12:55:16 -0500

    2/6

---
+++ Participant.py
@@ -0,0 +1,18 @@
+
+
+class Experiment:
+    def __init__(self, firstPanas, secondPanas, o2, eeg):
+        self.firstPanas = firstPanas
+        self.secondPanas = secondPanas
+        self.o2 = o2
+        self.eeg = eeg
+
+
+
+
+class Participant:
+    def __int__(self, id, first, second):
+        self.id = id
+        self.first = first
+        self.second = second
+
