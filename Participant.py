31b4e6d5015b7affd6175273f46f85b4f3d21bcd
nwhalen <nwhalen@wpi.edu>
2023-02-14 13:15:35 -0500

    2/14

--- Participant.py
+++ Participant.py
@@ -1,3 +1,8 @@
+import csv
+import sys
+import pandas as pd
+import typer
+from numpy import double
 
 
 class Experiment:
@@ -8,11 +13,43 @@ class Experiment:
         self.eeg = eeg
 
 
-
-
 class Participant:
     def __int__(self, id, first, second):
         self.id = id
         self.first = first
         self.second = second
+app = typer.Typer()
 
+
+
+@app.command()
+def readPANAS():
+    filePath = input("Enter CSV filepath:\n")
+    f = open(filePath, 'r')
+    reader = csv.reader(f)
+    results = {}
+
+    for row in reader:
+        results[row[0]] = {'PANAS_PA': (double(row[1])) / 10, 'PANAS_NA': (double(row[2])) / 10,
+                           'PANAS_SN': (double(row[3])) / 3}
+
+    print(f"results: {results}")
+
+
+@app.command()
+def readEEG():
+    filePath = input("Enter H5 filepath:\n")
+    # f = open(filePath, 'r')
+    results = {}
+    # fpath = sys.argv[1]
+    # if len(sys.argv) > 2:
+    # key = sys.argv[2]
+    # df = pd.read_hdf(fpath, key=key)
+    # else:
+    df = pd.read_hdf(filePath)
+    print(f"Finish")
+    df.to_csv(results, index=False)
+    print(f"results: {results}")
+
+if __name__ == "__main__":
+    app()
