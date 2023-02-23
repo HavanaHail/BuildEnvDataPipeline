86a9c4a9ab93bb15a47cc7b9d6e43a774be0060d
PaytonaDean <padean@wpi.edu>
2023-02-23 14:00:46 -0500

    sync

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
 import struct
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy import nan
 
 import typer
 
@@ -27,7 +29,7 @@ rows = []
 heartRate = []
 clock = []
 ave = []
-with open("O2Ring_20221118173026_OXIRecord.csv", 'r') as file:
+with open("O2Ring_20230213132922.csv", 'r') as file:
     csvreader = csv.reader(file)
     header = next(csvreader)
     for row in csvreader:
@@ -37,37 +39,44 @@ print(header)
 mean = 0
 count = 0
 watch = 0
-
+lastbmp = 0
 #Check for motion
 for time, sp02, pulse, motion, sp02reminder, pulseReminder in rows:  
     #Turn each row into a list of each thing in row 
-    print (time, sp02, pulse, motion, sp02reminder, pulseReminder)
+   # print (time, sp02, pulse, motion, sp02reminder, pulseReminder)
     if int(pulse)>60 and int(pulse)<100:
-        if int(pulse)< lastbmp+7 and int(pulse)>lastbmp-7:
+        #if int(pulse)<= lastbmp+7 and int(pulse)>=lastbmp-7 or lastbmp == 0:
+            print(watch, pulse)
             mean += int(pulse)
             count +=1
             heartRate.append(int(pulse))
             clock.append(watch)
+        #else:
+         #   heartRate.append(np.nan)
+          #  clock.append(watch)
+    else:
+        heartRate.append(np.nan)
+        clock.append(watch)
     watch +=4
     lastbmp = int(pulse)
   #  paresedRow = row.split(",")
     #print(row)
 ave = int(mean/count)
 #writing in new cleaned data
-fields = ['time','heartrate']
-name = "CleanedData" + ".csv"
-with open(name,'w') as csvfile:
-    for pulse in heartRate:
+#fields = ['time','heartrate']
+#name = "CleanedData" + ".csv"
+#with open(name,'w') as csvfile:
+#    for pulse in heartRate:
         
-
-#print("Pulse average is: ", ave)
-#plt.plot(clock,heartRate, label = 'Pulse')
-#plt.axhline(y=ave, color = 'r', linestyle = 'dashed', label = 'Average')
+#Put NA when the number bad don't cut it 
+print("Pulse average is: ", ave)
+plt.plot(clock,heartRate, label = 'Pulse')
+plt.axhline(y=ave, color = 'r', linestyle = 'dashed', label = 'Average')
 #plt.axvline(x=120,color ='g',linestyle ='dashed')
 #plt.axvline(x=300,color ='g',linestyle ='dashed')
 # plt.axvline(x=800,color ='g',linestyle ='dashed')
-# plt.xlabel('time (secs)')
-# plt.ylabel('heartrate')
-# plt.xticks(rotation=90,fontsize='small')
-# plt.title('participant #')
-# plt.show()
+plt.xlabel('time (secs)')
+plt.ylabel('heartrate')
+plt.xticks(rotation=90,fontsize='small')
+plt.title('participant #')
+plt.show()
