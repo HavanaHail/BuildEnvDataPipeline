6c1333a2ca8a0f87c4181354ed043f43e616b2d1
nwhalen <nwhalen@wpi.edu>
2023-03-20 14:05:00 -0400

    Merge remote-tracking branch 'origin/main'

--- Experiment.py
+++ Experiment.py
@@ -1,6 +1,8 @@
 import csv
 import sys,os
-import pandas
+
+import numpy as np
+import pandas as pd
 
 from numpy import double
 
@@ -18,17 +20,17 @@ def createExperiment():
     #idNum = sys.argv[1]
     #folder_path = sys.argv[2]
 
-    idNum = 1
+    idNum = 1856
     folder_path = '/Users/nwhalen/Developer/MQP/BuildEnvDataPipeline/EMILY'
     #target_path = '/Users/nwhalen/Developer/MQP/BuildEnvDataPipeline/EMILY'
-    panasCount = 1
-    target_path = os.path.join(folder_path, "Result")
+    panasCount = 0
+    target_path = os.path.join(folder_path, "Result" + str(idNum))
     if not os.path.exists(target_path):
         os.mkdir(target_path)
 
     for file in os.listdir(folder_path):
         f = os.path.join(folder_path, file)
-        if  os.path.isfile(f):
+        if os.path.isfile(f): ##CHECKS IN RESULT FOLDER - NOT GOOD
             #print(os.path.basename(f))
             if  'O2' in os.path.basename(f):
                 #Process o2 data Todo
@@ -39,45 +41,60 @@ def createExperiment():
                 print(os.path.basename(f))
             elif 'PANAS' in os.path.basename(f):
                     #Process panas data Todo
-                PANAS_DAT = readPANAS2(f)
-                print(PANAS_DAT)
-                    ##ADD TO FOLDER
-                target = target_path + '/' + 'PANAS' + '1' +'.csv' ##Panas number hardcoded for now
-                print(target)
-                PANAS_RES = panasToCSV(target, PANAS_DAT)
-
-                panasCount = panasCount + 1
-                print(os.path.basename(f))
+                if(panasCount < 2):
+                    data = getPanasScores("/Users/nwhalen/Developer/MQP/BuildEnvDataPipeline/CSV/test/CSVTEST_PANAS.csv", 1856)
+                    panasCount = panasCount + 1
+                    target = target_path + '/' + 'PANAS' + str(panasCount) +'.csv' ##Panas number hardcoded for now
+                    pd.DataFrame(data).to_csv(target, index_label="i", header=['Time/Date','PANAS_PA','PANAS_NA','PANAS_SN','ID'])
 
 
 
-def readPANAS(filePath):
-    #filePath = input("Enter CSV filepath:\n")
-    f = open(filePath, 'r')
-    reader = csv.reader(f)
-    results = {}
 
-    for row in reader:
-        results[row[0]] = {'PANAS_PA': (double(row[1])) / 10, 'PANAS_NA': (double(row[2])) / 10,
-                           'PANAS_SN': (double(row[3])) / 3}
+# def readPANAS(filePath):
+#     #filePath = input("Enter CSV filepath:\n")
+#     f = open(filePath, 'r')
+#     reader = csv.reader(f)
+#     results = {}
+#
+#     for row in reader:
+#         results[row[0]] = {'PANAS_PA': (double(row[1])) / 10, 'PANAS_NA': (double(row[2])) / 10,
+#                            'PANAS_SN': (double(row[3])) / 3}
+#
+#     #print(f"results: {results}")
+#     return results
+# def readPANAS2(filePath):
+#     with open(filePath, 'r') as data:
+#         for line in csv.reader(data):
+#             print(line)
+#         return data
 
-    #print(f"results: {results}")
-    return results
-def readPANAS2(filePath):
-    with open(filePath, 'r') as data:
-        for line in csv.reader(data):
-            print(line)
-        return data
+# def panasToCSV(target, dict):
+#     field_names = ['PANAS_PA', 'PANAS_NA', 'PANAS_SN']
+#
+#     with open(target, 'w') as csvFile:
+#         print(target)
+#         writer = csv.DictWriter(csvFile, fieldnames=field_names)
+#         writer.writeheader()
+#        # print(dict)
+#         writer.writerows(dict) ##NOT WORKING - I/O operation on closed file
+#         return csvFile
 
-def panasToCSV(target, dict):
-    field_names = ['PANAS_PA', 'PANAS_NA', 'PANAS_SN']
+def getPanasScores(target, idNum):
+    csv_file = csv.reader(open(target, "r"), delimiter=",")
+    panasCount = 1
+    array = []
 
-    with open(target, 'w') as csvFile:
-        print(target)
-        writer = csv.DictWriter(csvFile, fieldnames=field_names)
-        writer.writeheader()
-        print(dict)
-        writer.writerows(dict) ##NOT WORKING
+    for row in csv_file:
+        strId = str(idNum)
+        # if current rows 2nd value is equal to input, print that row
+        if strId == row[4]:
+            #print(row)
+            array.append(row)
+    #print(array)
+    result = np.asarray(array)
+    return result
+
+
 
 if __name__ == '__main__':
     createExperiment()
